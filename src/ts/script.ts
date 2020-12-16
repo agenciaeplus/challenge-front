0eff8eb4297c803d67870ba0ccbe4b44e4fb4584
Alan Vinicius Silva <alanvinicios@gmail.com>
2020-12-16 19:51:58 -0300

    Layout do carrinho sendo exibido

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,22 +1,50 @@
+// Elements
+const $cart = document.getElementById('cart');
+const $cartContent = document.getElementById('cart-content');
 
 
 
 
 
+// Configura os eventos que exibem e escondem o conteúdo do carrinho
+$cart.addEventListener('click', async event => {
+    toggleCartView();
+    event.stopPropagation();
+});
+
+$cartContent.addEventListener('click', event => {
+    event.stopPropagation();
+});
+
+document.addEventListener('click', event => {
+    toggleCartView(true);
+});
+//-------------------------------------------------------------------
 
 
+// Alterna a visibilidade do conteúdo do carrinho ou torna invisível se 'flag' for true
+// Retorna o estado: true => Visível | false => escondido
+function toggleCartView(flag?: boolean){
+    if(flag){
+        $cartContent.style.display = 'none';
+        return false;
+    }
+    else{
+        if($cartContent.style.display == 'none'){
+            $cartContent.style.display = 'inline-block';
+            return true;
+        }
+        else{
+            $cartContent.style.display = 'none';
+            return false;
+        }
+    }
+}
 
-
-
-
-
-// (async () => {
-//     const data = await fetch("data/products.json", {
-//         headers : { 
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json'
-//         }
-//     });
-    
-//     document.getElementById("content").innerText = JSON.stringify(await data.json(), null, 2);
-// })();
+// Faz uma requisição e retorna o conteúdo do carrinho
+async function requestCartData(){
+    return new Promise(async (resolve, reject) => {
+        const data = await fetch("data/products.json");
+        resolve(await data.json());
+    });
+}
