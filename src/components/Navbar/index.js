7c3605faa413a9af78462a78986400175cd246ad
Renan Paixão <renanlemos21@gmail.com>
2021-07-28 00:23:41 -0300

    feat(isMenuOpen): adição de funcionalidade para abrir o menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import Container from './styles';
+import MenuAnchor from './components/MenuAnchor';
 
 import Logo from '../../assets/images/logo/agencia-eplus-n-logo.png';
 import Menu from '../../assets/images/icons/menu-hamburguer.svg';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  function isMenuOpen() {
+    setMenuOpen(!menuOpen);
+  }
   return (
-    <Container>
-      <img id="logo" src={Logo} alt="Logo" />
-      <img id="menu" src={Menu} alt="Menu"></img>
-    </Container>
+    <>
+      <Container>
+        <img id="logo" src={Logo} alt="Logo" />
+        <img id="menu" src={Menu} alt="Menu" onClick={() => isMenuOpen()} />
+      </Container>
+      {menuOpen && <MenuAnchor />}
+    </>
   );
 }
 
