8425a81a9a12bcb465031fe68db6a5d7b486255d
Renan Paixão <renanlemos21@gmail.com>
2021-07-28 00:22:05 -0300

    feat(menu-anchor): criação de component MenuAnchor

---
+++ src/components/Navbar/components/MenuAnchor/index.js
@@ -0,0 +1,15 @@
+import Wrapper from './style';
+
+function MenuAnchor() {
+  return (
+    <Wrapper>
+      <li>Lorem Ipsum</li>
+      <li>Lorem Ipsum</li>
+      <li>Lorem Ipsum</li>
+      <li>Lorem Ipsum</li>
+      <li>Lorem Ipsum</li>
+    </Wrapper>
+  );
+}
+
+export default MenuAnchor;
---
+++ src/components/Navbar/components/MenuAnchor/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.ul`
+  font-size: 1.4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem 0;
+
+  background-color: var(--white);
+
+  li {
+    margin: 0.6rem 0;
+  }
+`;
+export default Wrapper;
