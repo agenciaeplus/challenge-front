4d7a1deaab2b602d99e59077b2ee5e91f09d8088
Renan Paixão <renanlemos21@gmail.com>
2021-07-29 04:10:56 -0300

    refactor(navbar): aumentando margem da logo no desktop

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -19,6 +19,7 @@ const Container = styled.nav`
     width: 7rem;
 
     margin-left: 0.5rem;
+
     @media (min-width: 300px) {
       margin-left: 1rem;
     }
@@ -29,8 +30,12 @@ const Container = styled.nav`
     }
     @media (min-width: 700px) {
       width: 14.4rem;
-      margin-left: 0.5rem;
+      margin-left: 1.5rem;
     }
+    @media (min-width: 900px) and (max-width: 999px) {
+      margin-left: 0.5px;
+    }
+
     @media (min-width: 1000px) {
       width: 16.4rem;
       height: 3rem;
