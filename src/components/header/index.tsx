b1309b49841d0553391bede9c96d7a16c73d1261
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-05-11 20:39:33 -0300

    styled Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,31 @@
+import './styles.scss';
+
+import logoIMG from '../../assets/images/logo.png';
+import searchIMG from '../../assets/images/search.svg';
+import personIMG from '../../assets/images/person.svg';
+import cartIMG from '../../assets/images/cart.svg';
+
 export function Header() {
   return (
-    <h1>Teste</h1>
+    <header className="container">
+      <div className="content">
+        <img src={logoIMG} alt="E Plus" />
+
+        <nav>
+          <a href="#">Lorem ipsum</a>
+          <a href="#">Lorem ipsum</a>
+          <a href="#">Lorem ipsum</a>
+          <a href="#">Lorem ipsum</a>
+          <a href="#">Lorem ipsum</a>
+        </nav>
+
+        <div className="icons">
+          <img src={searchIMG} alt="Pesquisar" />
+          <img src={personIMG} alt="Perfil" />
+          <img src={cartIMG} alt="Carrinho de compras" />
+        </div>
+
+      </div>
+    </header>
   );
 }
