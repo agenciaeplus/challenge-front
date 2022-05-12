e1756f929adcf24e6b2cffd2eae795c725885c0a
Luan Argolo <ipsluan@icloud.com>
2022-05-12 10:38:28 -0300

    :sparkles: feat: puxar dados da API

--- src/components/Produtos/index.js
+++ src/components/Produtos/index.js
@@ -1,53 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
-import notebook from '../../assets/images/products/note02-samsung-160-160.jpg';
-
 const Produtos = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('./products.json');
+      const json = await response.json();
+      setData(json);
+    }
+    getData();
+  }, []);
+
   return (
     <>
       <section className={styles.container}>
-        <div className={styles.content}>
-          <img src={notebook} alt="Notebook Samsung" />
-          <div className={styles.info}>
-            <h1>Notebook Samsung Intel Core</h1>
-            <div className={styles.info_preco}>
-              <p>Qtd.: 2</p>
-              <span>R$ 7.3232158,21</span>
+        {data &&
+          data.cart.item.map((item) => (
+            <div className={styles.content} key={item.productId}>
+              <img src={item.image} alt={item.name} />
+              <div className={styles.info}>
+                <h1>{item.name}</h1>
+                <div className={styles.info_preco}>
+                  <p>Qtd.: {item.quantity}</p>
+                  <span>R$ {item.bestPrice}</span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <img src={notebook} alt="Notebook Samsung" />
-          <div className={styles.info}>
-            <h1>Notebook Samsung Intel Core</h1>
-            <div className={styles.info_preco}>
-              <p>Qtd.: 2</p>
-              <span>R$ 7.3232158,21</span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <img src={notebook} alt="Notebook Samsung" />
-          <div className={styles.info}>
-            <h1>Notebook Samsung Intel Core</h1>
-            <div className={styles.info_preco}>
-              <p>Qtd.: 2</p>
-              <span>R$ 7.3232158,21</span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <img src={notebook} alt="Notebook Samsung" />
-          <div className={styles.info}>
-            <h1>Notebook Samsung Intel Core</h1>
-            <div className={styles.info_preco}>
-              <p>Qtd.: 2</p>
-              <span>R$ 7.3232158,21</span>
-            </div>
-          </div>
-        </div>
+          ))}
       </section>
+
       <div className={styles.total}>
         <p>
           Total do pedido: <span>R$ 20.356,95</span>
