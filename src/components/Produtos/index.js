2f86292de6bfce9fa0000d49e1279972fe60357e
Luan Argolo <ipsluan@icloud.com>
2022-05-12 08:41:05 -0300

    :sparkles: feat: esqueleto inicial dos produtos

--- src/components/Produtos/index.js
+++ src/components/Produtos/index.js
@@ -1,8 +1,63 @@
 import React from 'react';
 import styles from './styles.module.css';
 
+import notebook from '../../assets/images/products/note02-samsung-160-160.jpg';
+
 const Produtos = () => {
-  return <section>Produtos</section>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <img src={notebook} alt="Notebook Samsung" />
+        <div className={styles.info}>
+          <h1>Notebook Samsung Intel Core</h1>
+          <div className={styles.info_preco}>
+            <p>Qtd.: 1</p>
+            <span>R$ 7.158,21</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <img src={notebook} alt="Notebook Samsung" />
+        <div className={styles.info}>
+          <h1>Notebook Samsung Intel Core</h1>
+          <div className={styles.info_preco}>
+            <p>Qtd.: 1</p>
+            <span>R$ 7.158,21</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <img src={notebook} alt="Notebook Samsung" />
+        <div className={styles.info}>
+          <h1>Notebook Samsung Intel Core</h1>
+          <div className={styles.info_preco}>
+            <p>Qtd.: 1</p>
+            <span>R$ 7.158,21</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <img src={notebook} alt="Notebook Samsung" />
+        <div className={styles.info}>
+          <h1>Notebook Samsung Intel Core</h1>
+          <div className={styles.info_preco}>
+            <p>Qtd.: 1</p>
+            <span>R$ 7.158,21</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <img src={notebook} alt="Notebook Samsung" />
+        <div className={styles.info}>
+          <h1>Notebook Samsung Intel Core</h1>
+          <div className={styles.info_preco}>
+            <p>Qtd.: 1</p>
+            <span>R$ 7.158,21</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Produtos;
