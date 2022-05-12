cdd38432fc19c98e1512a9fa6b084ccc5e34d375
Luan Argolo <ipsluan@icloud.com>
2022-05-12 09:18:20 -0300

    :sparkles: feat: finalizar esqueleto e estrutura do site

--- src/components/Produtos/index.js
+++ src/components/Produtos/index.js
@@ -5,58 +5,58 @@ import notebook from '../../assets/images/products/note02-samsung-160-160.jpg';
 
 const Produtos = () => {
   return (
-    <section className={styles.container}>
-      <div className={styles.content}>
-        <img src={notebook} alt="Notebook Samsung" />
-        <div className={styles.info}>
-          <h1>Notebook Samsung Intel Core</h1>
-          <div className={styles.info_preco}>
-            <p>Qtd.: 1</p>
-            <span>R$ 7.158,21</span>
+    <>
+      <section className={styles.container}>
+        <div className={styles.content}>
+          <img src={notebook} alt="Notebook Samsung" />
+          <div className={styles.info}>
+            <h1>Notebook Samsung Intel Core</h1>
+            <div className={styles.info_preco}>
+              <p>Qtd.: 2</p>
+              <span>R$ 7.3232158,21</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <img src={notebook} alt="Notebook Samsung" />
-        <div className={styles.info}>
-          <h1>Notebook Samsung Intel Core</h1>
-          <div className={styles.info_preco}>
-            <p>Qtd.: 1</p>
-            <span>R$ 7.158,21</span>
+        <div className={styles.content}>
+          <img src={notebook} alt="Notebook Samsung" />
+          <div className={styles.info}>
+            <h1>Notebook Samsung Intel Core</h1>
+            <div className={styles.info_preco}>
+              <p>Qtd.: 2</p>
+              <span>R$ 7.3232158,21</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <img src={notebook} alt="Notebook Samsung" />
-        <div className={styles.info}>
-          <h1>Notebook Samsung Intel Core</h1>
-          <div className={styles.info_preco}>
-            <p>Qtd.: 1</p>
-            <span>R$ 7.158,21</span>
+        <div className={styles.content}>
+          <img src={notebook} alt="Notebook Samsung" />
+          <div className={styles.info}>
+            <h1>Notebook Samsung Intel Core</h1>
+            <div className={styles.info_preco}>
+              <p>Qtd.: 2</p>
+              <span>R$ 7.3232158,21</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <img src={notebook} alt="Notebook Samsung" />
-        <div className={styles.info}>
-          <h1>Notebook Samsung Intel Core</h1>
-          <div className={styles.info_preco}>
-            <p>Qtd.: 1</p>
-            <span>R$ 7.158,21</span>
+        <div className={styles.content}>
+          <img src={notebook} alt="Notebook Samsung" />
+          <div className={styles.info}>
+            <h1>Notebook Samsung Intel Core</h1>
+            <div className={styles.info_preco}>
+              <p>Qtd.: 2</p>
+              <span>R$ 7.3232158,21</span>
+            </div>
           </div>
         </div>
+      </section>
+      <div className={styles.total}>
+        <p>
+          Total do pedido: <span>R$ 20.356,95</span>
+        </p>
       </div>
-      <div className={styles.content}>
-        <img src={notebook} alt="Notebook Samsung" />
-        <div className={styles.info}>
-          <h1>Notebook Samsung Intel Core</h1>
-          <div className={styles.info_preco}>
-            <p>Qtd.: 1</p>
-            <span>R$ 7.158,21</span>
-          </div>
-        </div>
+      <div className={styles.finish}>
+        <p>Finalizar compra</p>
       </div>
-    </section>
+    </>
   );
 };
 
