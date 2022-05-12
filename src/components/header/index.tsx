46c367ccc7c49ca0b867364f6fad9336dddebfc9
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-05-11 22:42:07 -0300

    dropdown

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -23,7 +23,27 @@ export function Header() {
         <div className="icons">
           <img src={searchIMG} alt="Pesquisar" />
           <img src={personIMG} alt="Perfil" />
-          <img src={cartIMG} alt="Carrinho de compras" />
+
+          <div className="dropdown">
+            <button className="dropBtn">
+              <img src={cartIMG} alt="Carrinho de compras" />
+              <i className="fa fa-caret-down"></i>
+            </button>
+
+            <div className="dropdown-content">
+              <div className="items-cart">
+
+                <img src={logoIMG} alt="" />
+
+                <div className="text">
+                  <h4>Notebook Samsung e texto teste</h4>
+                  <p>Qtd: 1</p>
+                  <p>RS 7.158,21</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
 
       </div>
